3eafd30e386f1d9858ed2046d2ea13c91d45aaac
Thanasi Stratigakis <thanasi.strati@gmail.com>
2017-01-26 19:06:31 -0800

    factorial iterative working

--- recursion.py
+++ recursion.py
@@ -9,16 +9,23 @@ def factorial(n):
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
     # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
 
 
 def factorial_iterative(n):
     # Factorial function iteratively here
     fac = 1
-
-    for i in range(n, 1, -1):
-        fac *= i
-    return fac
+    # check if n is negative or not an integer (invalid input)
+    if n < 0 or not isinstance(n, int):
+        raise ValueError('factorial is undefined for n = {}'.format(n))
+    # check if n is one of the base cases
+    elif n == 0 or n == 1:
+        return 1
+    # check if n is an integer larger than the base cases
+    elif n > 1:
+        for i in range(1, n + 1):
+            fac *= i
+        return fac
 
     pass
     # once implemented, change factorial (above) to call factorial_iterative
