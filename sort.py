4a5673cf6a74fd97fb95ddd79ec4413e607a0da6
Thanasi Stratigakis <thanasi.strati@gmail.com>
2017-02-13 03:03:14 -0800

    selection sort

--- sort.py
+++ sort.py
@@ -12,6 +12,20 @@ def bubble_sort(items):
                 items[i + 1] = temp
     return items
 
+
+def selection_sort(items):
+    for i in range(0, len(items) - 1):
+        min_index = i
+        for a in range(i + 1, len(items)):
+            if items[a] < items[min_index]:
+                min_index = a
+        if min_index != i:
+            temp = items[min_index]
+            items[min_index] = items[i]
+            items[i] = temp
+    return items
+
+
 unsorted_list = [9,8,7,6,5,2,1,4]
 
-print(bubble_sort(unsorted_list))
+print(selection_sort(unsorted_list))
